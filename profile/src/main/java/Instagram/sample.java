db6350309d42312d67ff35b812f00e828b8e55bc
root <root@ip-172-31-27-83.eu-north-1.compute.internal>
2024-10-23 14:15:38 +0000

    mavenproject

---
+++ profile/src/main/java/Instagram/sample.java
@@ -0,0 +1,13 @@
+package Instagram;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Sravani" );
+    }
+}
